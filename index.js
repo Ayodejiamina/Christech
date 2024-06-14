ccb7b593aaddde7bccb69d0bf2bebf727698a4ce
Ayodeji amina <ayodjiamina.gmail.com>
2024-06-14 16:51:14 -0700

    christech solution portfolio

---
+++ index.js
@@ -0,0 +1,31 @@
+const showTab1 = document.querySelector('.tab1');
+const showTab2 = document.querySelector('.tab2');
+const showTab3 = document.querySelector('.tab3');
+const tab1 = document.querySelector('#first');
+const tab2 = document.querySelector('#second');
+const tab3 = document.querySelector('#third');
+const demo = document.querySelector('.demo');
+
+showTab1.addEventListener('click', () => {
+    tab1.classList.remove('hidden')
+    tab2.classList.add('hidden')
+    tab3.classList.add('hidden')
+    demo.style.display = 'none'
+
+})
+showTab2.addEventListener('click', () => {
+    tab1.classList.add('hidden')
+    tab2.classList.remove('hidden')
+    tab3.classList.add('hidden')
+    demo.style.display = 'none'
+
+
+})
+showTab3.addEventListener('click', () => {
+    tab1.classList.add('hidden')
+    tab2.classList.add('hidden')
+    tab3.classList.remove('hidden')
+    demo.style.display = 'none'
+
+
+})
